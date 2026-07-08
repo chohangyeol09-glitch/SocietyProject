2222b4b8bfe8c27b67dd4bc9b8c28eb03863088d
chohangyeol09 <chohangyeol09@gmail.com>
2026-07-08 19:55:07 +0900

    feat : asset

---
+++ Assets/CHG/GameModule/BicycleSystem/Scripts/Examples&Extras/BikeControlsExample.cs
@@ -0,0 +1,81 @@
+using CHG.Scripts;
+using rayzngames;
+using UnityEngine;
+
+namespace rayzngames
+{        
+    public class BikeControlsExample : MonoBehaviour
+    {
+        BicycleVehicle bicycle;
+        public bool controllingBike;
+        [SerializeField] private PlayerInputSO playerInput;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Awake()
+        {
+            bicycle = GetComponent<BicycleVehicle>();
+            playerInput.OnMoved += HandleMoved;
+            playerInput.OnStopStated += HandleStopStated;
+            playerInput.OnStopEnded += HandleStopEnded;
+        }
+
+        
+
+        private void HandleMoved(Vector2 evt)
+        {
+            bicycle.VerticalInput
+        }
+
+        private void HandleStopStated()
+        {
+            bicycle.Braking = true;
+        }
+        
+        private void HandleStopEnded()
+        {
+            bicycle.Braking = false;
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            bicycle.VerticalInput = Input.GetAxis("Vertical");
+            bicycle.HorizontalInput = Input.GetAxis("Horizontal");
+            BrakingInput();
+
+            //Extending functionality 
+            bicycle.InControl(controllingBike);
+
+            if (controllingBike)
+            {
+                //Constrains the Z rotation of the bike, when onground, and releases it when airborne.
+                bicycle.ConstrainRotation(bicycle.OnGround());
+            }
+            else
+            {
+                bicycle.ConstrainRotation(false);
+            }
+
+            /*
+            //Detach controls
+            if (bicycle.OnGround() == false) { controllingBike = false; }
+
+            //Landing Controls (Land Pressing E)
+            if (Input.GetKey(KeyCode.E)) { controllingBike = true; }
+            bicycle.InControl(controllingBike);   
+            */
+        }
+        void BrakingInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Braking");
+                
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                
+            }
+
+        }
+    }
+}
